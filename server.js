e0a2e8759c5d39fa25236cd10d3a3de7f7c5791d
Raegan Millhollin <desdemonhu@gmail.com>
2017-11-24 15:43:43 -0800

    express server works

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 'use strict';
+const express = require('express');
+const app = express();
+const router = require('./lib/_server.js');
+const PORT = process.env.PORT || 3000;
 
-const server = require('./lib/_server.js');
-const PORT = 3000;
+app.use('/', router);
 
-server.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Listening on port Number: ${PORT}`);
+});
