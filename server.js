d71015a2056039b169be9deeaaa6bc66a42d6a19
Raegan Millhollin <desdemonhu@gmail.com>
2017-11-24 18:40:08 -0800

    get, post, and put routes done for all tasks

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const router = require('./lib/_server.js');
 const PORT = process.env.PORT || 3000;
 
 app.use('/', router);
+app.use('/tasks/*', router);
 
 app.listen(PORT, () => {
   console.log(`Listening on port Number: ${PORT}`);
